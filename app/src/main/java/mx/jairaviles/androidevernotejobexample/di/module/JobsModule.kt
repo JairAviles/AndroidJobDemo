21e44475e51034c940b948c9a777e50716c9439d
Jair Avilés <hi@jairaviles.mx>
2018-01-18 23:06:55 -0200

    fix minor issues

--- app/src/main/java/mx/jairaviles/androidevernotejobexample/di/module/JobsModule.kt
+++ app/src/main/java/mx/jairaviles/androidevernotejobexample/di/module/JobsModule.kt
@@ -2,8 +2,6 @@ package mx.jairaviles.androidevernotejobexample.di.module
 
 import android.app.Application
 import com.evernote.android.job.Job
-import com.evernote.android.job.JobApi
-import com.evernote.android.job.JobConfig
 import com.evernote.android.job.JobManager
 import dagger.Module
 import dagger.Provides
@@ -20,8 +18,6 @@ class JobsModule {
     @Provides
     @Singleton
     fun providesManager(mApplication: Application, appJobCreator: AppJobCreator) : JobManager {
-        JobConfig.setForceAllowApi14(true)
-        JobConfig.setApiEnabled(JobApi.GCM, false) // is only important for Android 4.X
         JobManager.create(mApplication).addJobCreator(appJobCreator)
         return JobManager.instance()
     }
